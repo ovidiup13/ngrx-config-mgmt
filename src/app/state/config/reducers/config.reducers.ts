980f492288b1dbd03c04f9973788b4f561cc9233
Ovidiu Popoviciu <devovip13@gmail.com>
2018-05-25 22:34:28 +0300

    Using cryptoJS for sensitive keys

--- src/app/state/config/reducers/config.reducers.ts
+++ src/app/state/config/reducers/config.reducers.ts
@@ -6,20 +6,34 @@ import {
 } from "../actions";
 import { EnvConfig } from "../models/env-config.model";
 
+import { environment } from "../../../../environments/environment";
+
+import * as CryptoJS from "crypto-js";
+
 export interface State {
   serverApi: string;
+  secretApiKey: string;
 }
 
 export const initialState: State = {
-  serverApi: ""
+  serverApi: "",
+  secretApiKey: ""
 };
 
 export function reducer(state = initialState, action: Action) {
   switch (action.type) {
     case ConfigActionTypes.INIT_SUCCESSFUL: {
       const payload = (action as ConfigInitialiseSuccess).payload;
+
+      // decrypt config key
+      const secretApiKey = CryptoJS.AES.decrypt(
+        payload["ENCRYPTED_API_KEY"],
+        environment.secretMessage
+      ).toString(CryptoJS.enc.Utf8);
+
       return {
-        serverApi: payload["SERVER_API"]
+        serverApi: payload["SERVER_API"],
+        secretApiKey
       };
     }
     default: {
